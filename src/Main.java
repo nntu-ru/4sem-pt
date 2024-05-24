e3e64edfba34d9944e3c090161e308a3a34ea43f
Jolly Roger <enderg_official@mail.ru>
2024-05-24 20:47:41 +0300

    lab02

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Точка входа приложения
- */
-public class Main {
-    public static void main(String[] args) {
-        App app = new App();
-        app.run();
-    }
-}
--- src/organizer/iface/ISetter.java
+++
@@ -1,8 +0,0 @@
-package organizer.iface;
-
-/**
- * Интерфейс для функции organizer.Task::enter
- */
-public interface ISetter {
-    boolean trySet(String line);
-}
